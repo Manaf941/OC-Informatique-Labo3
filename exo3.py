2bc6af9c845587e4ff4c865ac83bfcbf043424d0
Manaf941 <manaf@2221.ch>
2023-12-15 17:57:54 +0100

    rename function in exo3

--- exo3.py
+++ exo3.py
@@ -1,6 +1,6 @@
 vowels = set("aeiouy")
 
-def extract_vowels(word: str):
+def count_vowels(word: str):
     count = 0
     for char in word:
         if char not in vowels:
@@ -11,4 +11,4 @@ def extract_vowels(word: str):
     return count
 
 for word in ["aujourd'hui", "exceptionnel", "cygne"]:
-    print(word, extract_vowels(word))
+    print(word, count_vowels(word))
